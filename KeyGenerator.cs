9235e0729795360f5899f21ecc2d9193aaa2a376
Michael <Michael@DESKTOP-GB0ULVC>
2022-02-12 16:56:08 +0100

    Projektdateien hinzufügen.

---
+++ KeyGenerator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    internal class KeyGenerator
+    {
+        public BigInteger PublicKey { get; private set; }
+        public BigInteger Totient { get; private set; }
+        public BigInteger PrivateKey { get; private set; }
+        public BigInteger Product { get; private set; }
+
+
+        public KeyGenerator(BigInteger p, BigInteger q)
+        {
+            Totient = GenerateTotient(p, q);
+            Product = p * q;
+            PublicKey = GeneratePublicKey(Totient);
+            PrivateKey = GeneratePrivateKey(Totient, PublicKey);
+        }
+
+        public BigInteger GeneratePrivateKey(BigInteger totient, BigInteger publicKey)
+        {
+            BigInteger result = 0;
+
+            while (true)
+            {
+                if (((result * publicKey) % totient) == 1)
+                {
+                    break;
+                }
+                result++;
+            }
+
+            return result;
+        }
+
+        public BigInteger GeneratePublicKey(BigInteger totient)
+        {
+            for (BigInteger publicKey = totient - 1; publicKey > 0; publicKey--)
+            {
+                if ((IsPrimeNumber(publicKey)) && (totient % publicKey != 0))
+                {
+                    return publicKey;
+                }
+            }
+
+            throw new Exception("No public key found");
+        }
+
+        public BigInteger GenerateTotient(BigInteger p, BigInteger q)
+        {
+            if (!IsPrimeNumber(p))
+            {
+                throw new Exception("p is not a prime number");
+            }
+
+            if (!IsPrimeNumber(q))
+            {
+                throw new Exception("q is not a prime number");
+            }
+            return ((p - 1) * (q - 1));
+        }
+
+        public bool IsPrimeNumber(BigInteger p)
+        {
+            BigInteger counter = 0;
+            for (BigInteger i = 1; i <= p; i++)
+            {
+                if (p % i == 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string EncryptMessage(string message)
+        {
+            long[] messageInArray = message.Select(c => (long)c).ToArray();
+            long[] encryptedMessageInArray = new long[messageInArray.Length];
+
+            for (int i = 0; i < messageInArray.Length; i++)
+            {
+                BigInteger buffer = IntPow(messageInArray[i], PublicKey);
+                buffer = buffer % Product;
+                encryptedMessageInArray[i] = (long)buffer;
+            }
+
+            message = new string(encryptedMessageInArray.Select(b => (char)b).ToArray());
+
+            return message;
+        }
+
+        public string DecryptMessage(string message)
+        {
+            long[] encryptedMessageInArray = message.Select(c => (long)c).ToArray();
+            long[] messageInArray = new long[encryptedMessageInArray.Length];
+
+            for (int i = 0; i < encryptedMessageInArray.Length; i++)
+            {
+                BigInteger buffer = IntPow(encryptedMessageInArray[i], PrivateKey);
+                buffer = buffer % Product;
+                messageInArray[i] = (long)buffer;
+            }
+
+            message = new string(messageInArray.Select(b => (char)b).ToArray());
+            return message;
+        }
+
+        private BigInteger IntPow(BigInteger baseInt, BigInteger exponent)
+        {
+            if (exponent < 0 )
+            {
+                throw new Exception("No negative numbers in exponent allowed");
+            }
+
+            BigInteger result = 1;
+            for (BigInteger i = 0; i < exponent; i++)
+            {
+                result *= baseInt;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace RSA
+{
+    internal class Program
+    {
+        static KeyGenerator keyGenerator;
+        static void Main(string[] args)
+        {
+            PickNewValues();
+            while (true)
+            {
+                Console.WriteLine("Enter new message: n");
+                Console.WriteLine("Generate new keys: k");
+                Console.WriteLine("Quit program:      q");
+                switch (Console.ReadLine())
+                {
+                    case "k":
+                        PickNewValues();
+                        break;
+                    case "n":
+                        EnterNewMessage();
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }
+           
+        }
+        static void PickNewValues()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter p: ");
+                    int p = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter q: ");
+                    int q = int.Parse(Console.ReadLine());
+
+                    keyGenerator = new KeyGenerator(p, q);
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+        static void EnterNewMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter new message:");
+                    string message = keyGenerator.EncryptMessage(Console.ReadLine());
+                    Console.WriteLine("Message encrypted: " + message);
+                    Console.WriteLine("Message decrypted: " + keyGenerator.DecryptMessage(message));
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
